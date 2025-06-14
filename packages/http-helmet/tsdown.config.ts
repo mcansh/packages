0639eb476bb63b3bb9dd29885b730e84eff0ddcf
Logan McAnsh <logan@mcan.sh>
2025-06-13 20:08:30 -0400

    chore: tweak and sync up packages

--- packages/http-helmet/tsdown.config.ts
+++ packages/http-helmet/tsdown.config.ts
@@ -4,13 +4,15 @@ import pkgJson from "./package.json" with { type: "json" };
 let external = Object.keys(pkgJson.dependencies || {});
 
 export default defineConfig({
-  shims: true,
   entry: ["src/index.ts", "src/react.tsx"],
-  sourcemap: true,
-  external,
-  tsconfig: "./tsconfig.json",
   dts: true,
   format: ["cjs", "esm"],
+  tsconfig: "./tsconfig.json",
+  sourcemap: true,
+  clean: true,
+  publint: true,
+  attw: true,
+  external,
   platform: "neutral",
   copy() {
     return ["../../LICENSE", { from: "../../LICENSE", to: "LICENSE" }];
--- packages/url/tsdown.config.ts
+++ packages/url/tsdown.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from "tsdown";
+import pkgJson from "./package.json" with { type: "json" };
+
+let external = Object.keys(pkgJson.dependencies || {});
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  experimentalDts: true,
+  dts: true,
   format: ["esm"],
   tsconfig: "tsconfig.json",
   sourcemap: true,
   clean: true,
+  publint: true,
+  attw: true,
+  external,
+  platform: "neutral",
   copy() {
     return ["../../LICENSE", { from: "../../LICENSE", to: "LICENSE" }];
   },
